4dc71ce6d654257f0f6a0f272a7ed4d9e716b7d3
pipselfmade <sergo_vzp@mail.ru>
2023-04-30 13:53:02 +0300

    Исправлены замечания ревьюера

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.IncorrectIdException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,24 +22,24 @@ public class FilmController {
     private final Map<Integer, Film> films = new HashMap<>();
 
     @PostMapping
-    private Film addFilm(@RequestBody Film film) {
+    private Film addFilm(@Valid @RequestBody Film film) {
         validate(film);
         film.setId(++id);
         films.put(film.getId(), film);
-        log.debug("Film '" + film.getName() + "' added successfully");
+        log.info("Film '" + film.getName() + "' added successfully");
         return film;
     }
 
     @PutMapping
-    public Film updateFilm(@RequestBody Film film) throws ValidationException, IncorrectIdException {
+    public Film updateFilm(@Valid @RequestBody Film film) throws ValidationException, IncorrectIdException {
         validate(film);
 
         if (films.containsKey(film.getId())) {
             films.put(film.getId(), film);
-            log.debug("Film '" + film.getName() + "' updated successfully");
+            log.info("Film '" + film.getName() + "' updated successfully");
             return film;
         } else {
-            log.debug("Incorrect id");
+            log.error("Incorrect id");
             throw new IncorrectIdException("Incorrect id");
         }
     }
@@ -48,23 +50,23 @@ public class FilmController {
     }
 
     public static void validate(Film film) {
-        if (film.getName().isBlank()) {
-            log.debug("Validation error: Films name can't be blank");
+        if (StringUtils.isBlank(film.getName())) {
+            log.error("Validation error: Films name can't be blank");
             throw new ValidationException("Films name can't be blank");
         }
 
         if (film.getDescription().length() > 200) {
-            log.debug("Validation error: Films description can't be longer than 200 chars");
+            log.error("Validation error: Films description can't be longer than 200 chars");
             throw new ValidationException("Films description can't be longer than 200 chars");
         }
 
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            log.debug("Validation error: Films release date can't be before 28.12.1895");
+            log.error("Validation error: Films release date can't be before 28.12.1895");
             throw new ValidationException("Films release date can't be before 28.12.1895");
         }
 
         if (film.getDuration() < 0) {
-            log.debug("Validation error: Films duration can't be negative");
+            log.error("Validation error: Films duration can't be negative");
             throw new ValidationException("Films duration can't be negative");
         }
     }
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.IncorrectIdException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,24 +22,24 @@ public class UserController {
     private final Map<Integer, User> users = new HashMap<>();
 
     @PostMapping
-    private User createUser(@RequestBody User user) throws ValidationException {
+    private User createUser(@Valid @RequestBody User user) throws ValidationException {
         validate(user);
         user.setId(++id);
         users.put(user.getId(), user);
-        log.debug("User '" + user.getName() + "' created successfully");
+        log.info("User '" + user.getName() + "' created successfully");
         return user;
     }
 
     @PutMapping
-    public User updateUser(@RequestBody User user) throws ValidationException, IncorrectIdException {
+    public User updateUser(@Valid @RequestBody User user) throws ValidationException, IncorrectIdException {
         validate(user);
 
         if (users.containsKey(user.getId())) {
             users.put(user.getId(), user);
-            log.debug("User '" + user.getName() + "' updated successfully");
+            log.info("User '" + user.getName() + "' updated successfully");
             return user;
         } else {
-            log.debug("Incorrect id");
+            log.error("Incorrect id");
             throw new IncorrectIdException("Incorrect id");
         }
     }
@@ -48,32 +50,33 @@ public class UserController {
     }
 
     public static void validate(User user) {
-        if (user.getName() == null || user.getName().isBlank()) {
+        if (StringUtils.isBlank(user.getName())) {
+        //if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
         }
 
         if (user.getEmail().isBlank()) {
-            log.debug("Validation error: Email name can't be blank");
+            log.error("Validation error: Email name can't be blank");
             throw new ValidationException("Email name can't be blank");
         }
 
         if (!user.getEmail().contains("@")) {
-            log.debug("Validation error: Email name should contain '@'");
+            log.error("Validation error: Email name should contain '@'");
             throw new ValidationException("Email name should contain '@'");
         }
 
         if (user.getLogin().isBlank()) {
-            log.debug("Validation error: Login can't be blank");
+            log.error("Validation error: Login can't be blank");
             throw new ValidationException("Login can't be blank");
         }
 
         if (user.getLogin().contains(" ")) {
-            log.debug("Validation error: Login can't contain spaces");
+            log.error("Validation error: Login can't contain spaces");
             throw new ValidationException("Login can't contain spaces");
         }
 
         if (user.getBirthday().isAfter(LocalDate.now())) {
-            log.debug("Validation error: Users birthday can't be in the future");
+            log.error("Validation error: Users birthday can't be in the future");
             throw new ValidationException("Users birthday can't be in the future");
         }
     }
